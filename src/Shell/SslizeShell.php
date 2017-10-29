59a1e7e5c37e78fd6d20591a7b47a20468ea08a5
euonymus <euonymus0220@gmail.com>
2017-10-29 23:13:38 +0900

    http -> https

---
+++ src/Shell/SslizeShell.php
@@ -0,0 +1,48 @@
+<?
+namespace App\Shell;
+use Cake\Console\Shell;
+
+use Cake\Core\Configure;
+
+use Cake\Log\Log;
+use Cake\ORM\TableRegistry;
+use App\Model\Table\SubjectsTable;
+
+use Cake\Filesystem\File;
+
+use App\Utils\U;
+use App\Utils\GoogleSearch;
+use App\Utils\Wikipedia;
+
+class SslizeShell extends Shell
+{
+  public function startup()
+  {
+    Configure::write('Belongsto.lang',     'ja');
+    Configure::write('Belongsto.lang_eng', 'eng');
+    Configure::write('Belongsto.privacyMode', \App\Controller\AppController::PRIVACY_PUBLIC);
+    $this->Subjects = TableRegistry::get('Subjects');
+    $this->Relations = TableRegistry::get('Relations');
+  }
+
+  public function main()
+  {
+    $template = '%s: %s: <img src="%s" style="width:150px;height:150px;"> <img src="%s" style="width:150px;height:150px;"><br>';
+
+    require_once("ssl_candidates.php");
+
+    $file = new File('/Users/euonymus/Sites/belongsto/work/src/cakephp/src/Shell/https.csv');
+    $file_html = new File('/Users/euonymus/Sites/belongsto/work/src/cakephp/src/Shell/html.html');
+
+    foreach($ids as $id) {
+      $data = $this->Subjects->get($id);
+      $res = GoogleSearch::getFirstImageFromImageSearch($data->name);
+      if ($res) {
+	$arr = [$id, $data->name, $data->image_path, $res];
+	$file->append(implode(',', $arr) . "\n");
+	$file_html->append(sprintf($template, $id, $data->name, $data->image_path, $res) . "\n");
+      }
+    }
+
+  }
+}
